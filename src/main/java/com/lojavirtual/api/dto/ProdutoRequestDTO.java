4b68b5c80d2417f41ba0375e596c44129686318b
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-02-10 20:40:39 -0300

    adding Produto DTO

---
+++ src/main/java/com/lojavirtual/api/dto/ProdutoRequestDTO.java
@@ -0,0 +1,51 @@
+package com.lojavirtual.api.dto;
+
+import java.math.BigDecimal;
+
+public class ProdutoRequestDTO {
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private String categoria;
+    private int estoque;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
